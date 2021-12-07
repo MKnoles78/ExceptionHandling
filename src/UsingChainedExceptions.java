0f27c473de73eba364b5ba35df0bce442f42598e
Mike Knoles <MKnoles78@gmail.com>
2021-12-06 23:24:13 -0500

    added second method

--- src/UsingChainedExceptions.java
+++ src/UsingChainedExceptions.java
@@ -6,8 +6,17 @@ public class UsingChainedExceptions {
 			method1();
 		} catch (Exception exception) {
 			// exceptions thrown from method1
-			exceptions.printStackTrace();
+			exception.printStackTrace();
 		}
 	}
 
+	// call method2; throws exception back to main
+	public static void method1() throws Exception {
+		try {
+			method2();
+		} catch (Exception exception) {
+			// exception thrown from method2
+			throw new Exception("Exception thrown in method1", exception);
+		}
+	}
 }
