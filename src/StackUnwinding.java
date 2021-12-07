72a126a81165d7aa2dfdacb372d9906e18bc3328
Mike Knoles <MKnoles78@gmail.com>
2021-12-06 23:04:19 -0500

    added in loop to get exceptions

--- src/StackUnwinding.java
+++ src/StackUnwinding.java
@@ -2,7 +2,28 @@
 public class StackUnwinding {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			method1();
+		} catch (Exception exception) {
+			// catch exception thrown in method1
+			System.err.printf("%s%n%n", exception.getMessage());
+			exception.printStackTrace();
+
+			// obtain the stack-trace information
+			StackTraceElement[] traceElements = exception.getStackTrace();
+
+			System.out.printf("%nStack trace from getStrackTrace: %n");
+			System.out.println("Class\t\tFile\t\t\tLine\tMethod");
+
+			// loop through traceElements to get exception description
+			for (StackTraceElement element : traceElements) {
+				System.out.printf("%s\t", element.getClassName());
+				System.out.printf("%s\t", element.getFileName());
+				System.out.printf("%s\t", element.getLineNumber());
+				System.out.printf("%s\t", element.getMethodName());
+
+			}
+		}
 
 	}
 
